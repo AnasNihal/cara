d6df1c34c7913acf028c8a6cff84b674f839f1e0
Anas Nihal <anasnihal03@gmail.com>
2023-06-25 21:03:07 +0530

    Login page and other some changes

--- app.js
+++ app.js
@@ -11,3 +11,27 @@ if(close){
            nav.classList.remove('active');
     })
 }
+
+
+
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+
+registerLink.addEventListener("click", ()=> {
+    wrapper.classList.add('active');
+});
+loginLink.addEventListener("click", ()=> {
+    wrapper.classList.remove('active');
+});
+
+
+window.onload = function () {
+    document.getElementById("form").onsubmit = function () {
+        var filmName = document.getElementById("username").value;
+  
+        if (filmName == "anasnihal@gmail.com") window.location.href = "home.html";
+        else alert("Wrong Entry");
+        return false; //prevent the default behavior of the submit event
+    }
+} 
